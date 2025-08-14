8371a5a511f45aaeba5fcfbdaed62c2a06297099
DitaSetyaKurniawan-Droid <ditasetya@example.com>
2025-08-14 15:29:08 +0700

    new  commit, learn docker

--- docker2/index.js
+++ docker2/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hallo saya dita setya kurniawan');
+    res.send('Hallo saya dita setya kurniawan ya');
 });
 
 app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
-});
+}); 
